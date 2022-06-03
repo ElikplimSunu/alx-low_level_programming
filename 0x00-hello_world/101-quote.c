5c89e94c47d74624aee069bdb5e6fbbde6453d7c
Eric Elikplim Sunu <sunuerico@gmail.com>
2022-06-02 20:09:12 -0700

    Added a C program that writes to the stderr output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-//the backslash was used to split the long
-//line of code into two, so as to not violate
-//the Betty linting
+/*the backslash was used to split the long
+*line of code into two, so as to not violate
+*the Betty linting
+*/
 fprintf(stderr,\
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
